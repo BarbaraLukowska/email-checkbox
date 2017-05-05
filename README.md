86eb3483f1dd31d94aa7fa88d13af9805cbbf366
Barbara Łukowska <barbara.m.lukowska@gmail.com>
2017-05-05 11:47:29 +0200

    handleCheck event added: event on holded shift

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # email-checkbox
 Exercise: hold shift to check multiple checkboxes
+I've used: HTML, CSS, JS, ES6
--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+    let lastChecked;
+    function handleCheck(e) {
+        if (e.shiftKey) {
+            console.log(e)
+        }
+    }
 
+    checkboxes.forEach( checkbox => checkbox.addEventListener('click', handleCheck));
 });
